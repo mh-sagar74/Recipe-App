59f804e3f177f1f3c72de4ffdd7d3d107828952a
Mominul Haque Sagar <mominulhaquesagar@gmail.com>
2025-02-27 22:46:14 +0600

    creating about page

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export default function page() {
+  return (
+    <Box>
+      <Typography>
+        <span className="text-2xl font-semibold">About</span>
+      </Typography>
+    </Box>
+  );
+}
